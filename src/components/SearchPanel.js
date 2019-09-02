d2f1a0dd41bf744bfd90f0b15e549e55bb8fbf92
Denys Zabudskyi <Denys_Zabudskyi@govitall.com>
2019-09-02 15:50:20 +0300

    RP003 added new content

--- src/components/SearchPanel.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const SearchPanel = () => {
-    return <input placeholder="search" type="text"/>;
-};
-
-export default SearchPanel;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import AppHeader from "./components/AppHeader";
-import TodoList from "./components/TodoList";
-import SearchPanel from "./components/SearchPanel";
+import App from './components/app';
 
-const App = () => {
-
-    const todoData = [
-        {label: 'Dring Cofee', important: false},
-        {label: 'Cofee', important: true},
-        {label: 'Dring', important: false}
-    ];
-
-    return (
-        <div>
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList todos={todoData} />
-        </div>
-    );
-}
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
