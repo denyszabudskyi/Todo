80fa15fd1b47fec3bd0945aa35450f1ec3407e21
Denys Zabudskyi <Denys_Zabudskyi@govitall.com>
2019-10-09 12:30:25 +0300

    Updates

---
+++ precache-manifest.20fb3e3077d565a4f50c16a75837ba47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a0129bb30eb88e4bc28d7c011cfbfa",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "7620b0fa9388028122d6",
+    "url": "/Todo/static/css/main.e6b9b51d.chunk.css"
+  },
+  {
+    "revision": "9319774a87fb395a0206",
+    "url": "/Todo/static/js/2.d6ab339c.chunk.js"
+  },
+  {
+    "revision": "7620b0fa9388028122d6",
+    "url": "/Todo/static/js/main.b7af06c5.chunk.js"
+  },
+  {
+    "revision": "7ec7648bb679e645ec16",
+    "url": "/Todo/static/js/runtime~main.37ac2452.js"
+  }
+]);
